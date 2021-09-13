a5dbd56bb16b32e84a1e563296c5e375588e17b5
jeziel <jezielmarques7@gmail.com>
2021-09-13 11:08:48 -0300

    animate cart button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import { Feather } from '@expo/vector-icons';
 import { TouchableOpacity, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import { MotiView } from 'moti';
 
 import { styles } from './styles';
 import { theme } from '../../styles/theme';
 
 export function Button() {
   return (
-    <TouchableOpacity
-      style={styles.button}
-      activeOpacity={.8}
+    <MotiView
+      from={{
+        scale: 0,
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+        scale: [
+          { value: 0, type: 'timing' },
+          { value: 1.1, type: 'spring' },
+          { value: 1, type: 'timing' },
+        ],
+      }}
     >
-      <Text style={styles.text}>
-        Add to Card
-      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={.8}
+      >
+        <Text style={styles.text}>
+          Add to card
+        </Text>
 
-      <Feather
-        name="shopping-cart"
-        size={24}
-        color={theme.colors.white}
-      />
-    </TouchableOpacity>
+        <Feather
+          name="shopping-cart"
+          size={24}
+          color={theme.colors.white}
+        />
+      </TouchableOpacity>
+    </MotiView>
   );
 }
