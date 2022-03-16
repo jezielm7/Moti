79e4738fc9c8f9c3d79ad71aaa4796500d24192b
jeziel <jezielmarques7@gmail.com>
2022-03-16 01:05:47 -0300

    add animate-presence from motion

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,41 +2,43 @@ import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-import { MotiView } from 'moti';
+import { MotiView, AnimatePresence } from 'moti';
 
 import { styles } from './styles';
 import { theme } from '../../styles/theme';
 
 export function Button() {
   return (
-    <MotiView
-      from={{
-        scale: 0,
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-        scale: [
-          { value: 0, type: 'timing' },
-          { value: 1.1, type: 'spring' },
-          { value: 1, type: 'timing' },
-        ],
-      }}
-    >
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={.8}
+    <AnimatePresence>
+      <MotiView
+        from={{
+          scale: 0,
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          scale: [
+            { value: 0, type: 'timing' },
+            { value: 1.1, type: 'spring' },
+            { value: 1, type: 'timing' },
+          ],
+        }}
       >
-        <Text style={styles.text}>
-          Add to card
-        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={.8}
+        >
+          <Text style={styles.text}>
+            Add to card
+          </Text>
 
-        <Feather
-          name="shopping-cart"
-          size={24}
-          color={theme.colors.white}
-        />
-      </TouchableOpacity>
-    </MotiView>
+          <Feather
+            name="shopping-cart"
+            size={24}
+            color={theme.colors.white}
+          />
+        </TouchableOpacity>
+      </MotiView>
+    </AnimatePresence>
   );
 }
